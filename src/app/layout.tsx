662c9be692817c4e02c65e362a1697c93acb5c1e
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-03 18:29:05 +0530

    Fix the header path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import ThemeRegistry from "@/components/ThemeRegistry/ThemeRegistry";
-import ResponsiveAppBar from "@/components/Header";
+import ResponsiveAppBar from "@/components/Header/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
