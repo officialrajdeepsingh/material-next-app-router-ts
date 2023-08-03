70d68205010ee2e9b83b8670769a69bb52b945c1
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-03 19:05:02 +0530

    remove the unused component

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,7 +1,6 @@
 "use client"
 import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
-import Box from '@mui/material/Box';
 import { useTheme } from '@mui/material/styles';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
