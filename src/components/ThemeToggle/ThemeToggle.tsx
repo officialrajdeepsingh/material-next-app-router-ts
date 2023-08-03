28f314f08cbb712c799bb53a8b66a3ada9c700c1
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-03 18:26:09 +0530

    theme toggle ( Dark to light)

---
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -0,0 +1,21 @@
+"use client"
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { ColorModeContext } from './ThemeToggleProvider';
+
+export function ThemeToggle() {
+
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
+
+  return (
+      <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+      </IconButton>
+  );
+}
+
