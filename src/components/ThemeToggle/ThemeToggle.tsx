02341f45d9bb53e33509eed7a886d8c58717aacc
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-06 22:38:00 +0530

    add comment

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,18 +1,26 @@
 "use client"
-import * as React from 'react';
+
+import  React,{ useContext } from 'react';
 import IconButton from '@mui/material/IconButton';
 import { useTheme } from '@mui/material/styles';
+
+// import SVG ICONS from icons-material
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
-import { ColorModeContext } from './ThemeToggleProvider';
+
+// import theme toggle context
+import { ThemeToggleContext } from './ThemeToggleContext';
 
 export function ThemeToggle() {
 
+  // check theme is dark or light
   const theme = useTheme();
-  const colorMode = React.useContext(ColorModeContext);
+
+  // useContect Hook get value https://react.dev/reference/react/useContext
+  const themeToggle = useContext(ThemeToggleContext);
 
   return (
-      <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+      <IconButton sx={{ ml: 1 }} onClick={themeToggle.toggleColorMode} color="inherit">
         {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
       </IconButton>
   );
